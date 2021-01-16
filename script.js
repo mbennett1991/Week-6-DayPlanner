055aa162b3147388df658b171722e5fa1d238766
Elle <bound_by_harmony@yahoo.com>
2021-01-16 19:51:12 +0000

    Javascript Update

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-var m = moment();
+var currentDay = moment();
 
-$("#currentDay").text(m.format("dddd, DD MMMM, YYYY"));
+$("#currentDay").text(currentDay.format("dddd, DD MMMM, YYYY"));
 
-$(".hour").text()
+var hours = $(".hour");
+var toDos = [];
+var textArea = $(".description");
+var currentHour = moment().format("HH:mm:a")
+var saveBtn = $(".saveBtn")
+
+console.log(currentHour);
+//function that links hours to descriptions and moment.js
+function timeTrack(){
+
+    if (hours.value === currentHour){
+        //set this(keyword) to "style" ".present"
+    }else if (hours.value == currentHour.isBefore())
+        //set this to "style" ".past"
+    }else (hours.value == currentHour.isAfter()){
+        //set this to "style" ".future"
+};
+
+//onclick events that save textarea to local storage 
+
+saveBtn.on("click", function{
+    
+})
+
+
+
+var homeworkDue = moment("2021-01-17, 11:59:59");
+
+console.log(homeworkDue.diff(currentDay, "seconds"));
