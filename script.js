dbabde85d9709cf961754856bb40d0d4d882dc46
Elle <bound_by_harmony@yahoo.com>
2021-01-17 23:22:36 +0000

    Reorganised

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var currentDay = moment();
 
 $("#currentDay").text(currentDay.format("dddd, DD MMMM, YYYY"));
-
+//maybe try changing value to something else?
 var hours = $(".hour");
 var toDos = [];
 var textArea = $(".description");
 var currentHour = moment().format("HH:mm:A");
 var saveBtn = $(".saveBtn");
+var values = $("value")
 
 console.log(currentHour);
 //function that links hours to descriptions and moment.js
@@ -15,48 +16,47 @@ function timeTrack(){
     //parseInt hours value? 
     hours.each(function(){
     var thisHour = $(this);
-    var scheduleHour = parseInt(thisHour.attr(value));
+    var scheduleHour = parseInt(thisHour.attr(values));
     console.log(scheduleHour);
 
     if (scheduleHour === currentHour){
-        //set this(keyword) to "style" ".present"
-        hours.addClass("present");
-    }//else if (hours.value == currentHour.isBefore()){
-        //set this to "style" ".past"
-       // hours.addClass("past");
-   // }else (hours.value == currentHour.isAfter()){
+        thisHour.addClass("present");
+    }else if (currentHour.isAfter(scheduleHour)){
+        thisHour.addClass("past");
+    }//else (scheduleHour == currentHour.isAfter()){
         //set this to "style" ".future"
       //  hours.addClass("future");
 });
 timetrack();
 };
 
-/*
+
 timeTrack();
 //
 // insert for each 
 function savedToWindow(){
-    var retrieveTextArea = window.localStorage.getItem("textArea", textArea);
-    if (retrieveTextArea == null){
-        return;
-    } else {
-        textArea.text(retrieveTextArea);
-    }
-}
-
-//onclick events that save textarea to local storage 
-
-//insert for each THIS
 //you need to stringify and parse 
 saveBtn.on("click", function(event){
     event.preventDefault();
 
     localStorage.setItem("textArea", textArea);
     savedToWindow()
-});
+})
+};
+
+function getFromLocalStorage(){
+    textArea.each(function(){
+        var thisTextArea = $(this);
+        var retrieveTextArea = window.localStorage.getItem("textArea", thisTextArea);
+    if (retrieveTextArea == null){
+        return;
+    } else {
+        textArea.text(retrieveTextArea);
+    }}
+    )
+}
 
 
 var homeworkDue = moment("2021-01-17, 23:59:59");
 
 console.log(homeworkDue.diff(currentDay, "seconds"));
-*/
