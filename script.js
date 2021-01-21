fe67bb0d1de2dc426fc1febe06f38bee15d54c52
Elle <bound_by_harmony@yahoo.com>
2021-01-21 18:57:17 +0000

    Start Over JS file

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var currentDay = moment();
-
-$("#currentDay").text(currentDay.format("dddd, DD MMMM, YYYY"));
-//maybe try changing value to something else?
+var timeBlock = $(".time-block");
 var hours = $(".hour");
 var toDos = [];
 var textArea = $(".description");
@@ -9,58 +7,57 @@ var currentHour = moment().format("HH:mm:A");
 var saveBtn = $(".saveBtn");
 var values = $("value")
 
-console.log(currentHour);
-//function that links hours to descriptions and moment.js
+$("#currentDay").text(currentDay.format("dddd, DD MMMM, YYYY"));
+
 function timeTrack(){
-    //for each THIS
-    //parseInt hours value? 
-    hours.each(function(){
+    timeBlock.each(function(){
     var thisHour = $(this);
-    var scheduleHour = parseInt(thisHour.attr(values));
-    console.log(scheduleHour);
+    var scheduleHour = parseInt(thisHour.attr("data-hour"));
 
-    if (scheduleHour === currentHour){
-        thisHour.addClass("present");
-    } else if (currentHour < scheduleHour){
+    // if (scheduleHour === currentHour){
+    //     thisHour.addClass("present");
+    // } else 
+    if (currentHour < scheduleHour){
         thisHour.addClass("past");
-    } else (scheduleHour < currentHour){
-        thishour.addClass("future")
+    } else {
+        thishour.addClass("future");
     };
-);
-
-timetrack();
-};
-
-
-timeTrack();
-//
-// insert for each 
-function savedToWindow(){
-// link local storage to to-dos empty array    
-// save user input from textarea to todos
-// you need to stringify and parse
-// set/get local storage?
-saveBtn.on("click", function(event){
-    event.preventDefault();
-//Save to do to local storage
-    localStorage.setItem("textArea", textArea);
-    savedToWindow()
 })
 };
 
-function getFromLocalStorage(){
-    textArea.each(function(){
-        var thisTextArea = $(this);
-        var retrieveTextArea = window.localStorage.getItem("textArea", thisTextArea);
-    if (retrieveTextArea == null){
-        return;
-    } else {
-        textArea.text(retrieveTextArea);
-    }}
-    )
-}
+// timetrack();
+// };
 
 
-var homeworkDue = moment("2021-01-17, 23:59:59");
+// timeTrack();
+// //
+// // insert for each 
+// function savedToWindow(){
+// // link local storage to to-dos empty array    
+// // save user input from textarea to todos
+// // you need to stringify and parse
+// // set/get local storage?
+// saveBtn.on("click", function(event){
+//     event.preventDefault();
+// //Save to do to local storage
+//     localStorage.setItem("textArea", textArea);
+//     savedToWindow()
+// })
+// };
 
-console.log(homeworkDue.diff(currentDay, "seconds"));
+// function getFromLocalStorage(){
+//     textArea.each(function(){
+//         var thisTextArea = $(this);
+//         var retrieveTextArea = window.localStorage.getItem("textArea", thisTextArea);
+//     if (retrieveTextArea == null){
+//         return;
+//     } else {
+//         textArea.text(retrieveTextArea);
+//     }}
+//     )
+// }
+
+
+// var homeworkDue = moment("2021-01-17, 23:59:59");
+
+// console.log(homeworkDue.diff(currentDay, "seconds"));
